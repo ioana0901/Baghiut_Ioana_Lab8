18cc946943de63e80a600d89ffadd105567c8389
IOANA-ROXANA BĂGHIUȚ <ioana.baghiut@stud.ubbcluj.ro>
2020-11-28 13:48:51 +0200

    Add project files.

---
+++ Data/Baghiut_Ioana_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Baghiut_Ioana_Lab8.Models;
+
+namespace Baghiut_Ioana_Lab8.Data
+{
+    public class Baghiut_Ioana_Lab8Context : DbContext
+    {
+        public Baghiut_Ioana_Lab8Context (DbContextOptions<Baghiut_Ioana_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Baghiut_Ioana_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Baghiut_Ioana_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Baghiut_Ioana_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Baghiut_Ioana_Lab8.Data;
+using Baghiut_Ioana_Lab8.Models;
+
+namespace Baghiut_Ioana_Lab8.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Baghiut_Ioana_Lab8.Data.Baghiut_Ioana_Lab8Context _context;
+
+        public IndexModel(Baghiut_Ioana_Lab8.Data.Baghiut_Ioana_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Category> Category { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Category = await _context.Category.ToListAsync();
+        }
+    }
+}
